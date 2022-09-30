1a11bf42e4b8c57d367110194e8539c0099ea3bf
t33k00 <tmkyllonen@gmail.com>
2022-09-30 10:16:06 +0300

    newfinalversion

--- src/App.js
+++ src/App.js
@@ -1,24 +1,35 @@
-import logo from './logo.svg';
+import { useState } from 'react';
 import './App.css';
 
 function App() {
+
+  const [ika, setIka] = useState('')
+  const [yläraja, setYläraja] = useState('')
+  const [alaraja, setAlaraja] = useState('')
+
+
+  function laske(e){
+    e.preventDefault()
+    const ylaraja = (220-ika) * 0.85
+    const alaraja = (220-ika) * 0.65
+    setYläraja(ylaraja)
+    setAlaraja(alaraja)
+  }
+  
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+ <div id="content">
+  <h3> Sykerajat Laskuri</h3>
+  <form onSubmit={laske}>
+    <div> Ikä 
+    <input value={ika} onChange={e => setIka(e.target.value)}/>
     </div>
+    <div>
+      <label> syke rajat</label>
+      <output> {alaraja}-{yläraja}</output>
+    </div>
+    <button> Laske</button>
+  </form>
+ </div>
   );
 }
 
